4e0e66ba710e6f2cd79303ce19032900165d8648
Hamiora Keelan <Hamiora.Keelan@gmail.com>
2024-07-31 20:43:53 +1200

    add shadows

---
+++ client/styles/shadow.ts
@@ -0,0 +1,10 @@
+export const boxShadow = {
+    DEFAULT: "0 1px 3px 0 rgba(16, 24, 40, 0.10)",
+    sm: "0 1px 2px 0 rgba(16, 24, 40, 0.05)",
+    md: "0 4px 8px 0 rgba(16, 24, 40, 0.10)",
+    lg: "0 12px 16px -4px rgba(16, 24, 40, 0.08)",
+    xl: "0 20px 24px -4px rgba(16, 24, 40, 0.08)",
+    "2xl": "0 24px 48px -12px rgba(16, 24, 40, 0.18)",
+    "3xl": "0 32px 64px -12px rgba(16, 24, 40, 0.14)",
+  };
+  

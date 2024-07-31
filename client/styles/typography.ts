9e0d4104b84d8349cc305b80859e237420a3edb8
Hamiora Keelan <Hamiora.Keelan@gmail.com>
2024-07-31 20:42:27 +1200

    add color and typography

---
+++ client/styles/typography.ts
@@ -0,0 +1,45 @@
+export const typography = {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+    fontSize: {
+      xs: "0.75rem", // 12px
+      sm: "0.875rem", // 14px
+      base: "1rem", // 16px
+      lg: "1.125rem", // 18px
+      xl: "1.25rem", // 20px
+      "2xl": "1.5rem", // 24px
+      "3xl": "1.875rem", // 30px
+      "4xl": "2.25rem", // 36px
+      "5xl": "3rem", // 48px
+      "6xl": "3.75rem", // 60px
+      "7xl": "4.5rem", // 72px
+    },
+    fontWeight: {
+      thin: 100,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+      black: 900,
+    },
+    letterSpacing: {
+      normal: "0rem",
+      tightest: "-0.02rem",
+    },
+    lineHeight: {
+      1: "1.125rem", // 1.125 * 16px = 18px
+      2: "1.25rem", // 1.25 * 16px = 20px
+      3: "1.5rem", // 1.5 * 16px = 24px
+      4: "1.75rem", // 1.75 * 16px = 28px
+      5: "1.875rem", // 1.875 * 16px = 30px
+      6: "2rem", // 2 * 16px = 32px
+      7: "2.375rem", // 2.375 * 16px = 38px
+      8: "2.75rem", // 2.75 * 16px = 44px
+      9: "3.75rem", // 3.75 * 16px = 60px
+      10: "4.5rem", // 4.5 * 16px = 72px
+      11: "5.625rem",
+    },
+  };

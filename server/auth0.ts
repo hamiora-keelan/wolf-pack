a345477a3fef30e97c8bba2c8d93a38a16008e98
Hamiora Keelan <Hamiora.Keelan@gmail.com>
2024-08-01 13:45:27 +1200

    inital commit

--- server/auth0.ts
+++ server/auth0.ts
@@ -5,7 +5,7 @@ import { JwtPayload } from 'jsonwebtoken'
 import jwks from 'jwks-rsa'
 
 // TODO: set the domain and audience (API Identifier)
-const domain = 'https://'
+const domain = 'https://piwakwaka-hami.au.auth0.com'
 const audience = 'https://'
 
 const checkJwt = jwt({
@@ -27,3 +27,33 @@ export interface JwtRequest<TReq = any, TRes = any>
   extends Request<ParamsDictionary, TRes, TReq> {
   auth?: JwtPayload
 }
+
+// import { expressjwt as jwt, GetVerificationKey } from 'express-jwt'
+// import { Request } from 'express'
+// import { ParamsDictionary } from 'express-serve-static-core'
+// import { JwtPayload } from 'jsonwebtoken'
+// import jwks from 'jwks-rsa'
+
+// // TODO: set the domain and audience (API Identifier)
+// const domain = 'https://whai-2024-sam.au.auth0.com'
+// const audience = 'https://coordinary/api'
+
+// const checkJwt = jwt({
+//   secret: jwks.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: `${domain}/.well-known/jwks.json`,
+//   }) as GetVerificationKey,
+//   audience: audience,
+//   issuer: `${domain}/`,
+//   algorithms: ['RS256'],
+// })
+
+// export default checkJwt
+
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// export interface JwtRequest<TReq = any, TRes = any>
+//   extends Request<ParamsDictionary, TRes, TReq> {
+//   auth?: JwtPayload
+// }
